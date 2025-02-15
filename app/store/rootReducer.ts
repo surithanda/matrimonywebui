d9b7aabe9ef016488f4ff0152187a82e30c2feb5
b4xabhishek <b4xabhishek@gmail.com>
2025-02-15 21:10:54 +0530

    reducers go

--- app/store/rootReducer.ts
+++ app/store/rootReducer.ts
@@ -1,10 +1,12 @@
 import { combineReducers } from '@reduxjs/toolkit';
 import registerReducer from './features/registerSlice';
 import authReducer from './features/authSlice';
+import profileReducer from './features/profileSlice';
 
 const rootReducer = combineReducers({
   register: registerReducer,
-  auth: authReducer
+  auth: authReducer,
+  profile: profileReducer
 });
 
 export default rootReducer;

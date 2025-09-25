bbf6c9c8f64a4c439daaea111c06c9f9ac363447
Badal Kumar Nayak <b.nayak012@hotmail.com>
2025-09-25 17:17:38 +0530

    suucess modal ui fixed in payment page

--- app/(dashboard)/payments/_components/SuccessModal.tsx
+++ app/(dashboard)/payments/_components/SuccessModal.tsx
@@ -87,11 +87,11 @@ export default function PaymentStatusModal({
               <p className="text-2xl font-bold">{plan.price}</p>
               <div className="flex items-center gap-2 text-gray-500 mt-2 text-sm">
                 <Calendar className="w-4 h-4" />
-                <span>{plan.duration} from purchase</span>
+                <span>1 year (365 days from purchase)</span>
               </div>
             </div>
 
-            <div className="border rounded-xl p-4 bg-white shadow-sm">
+            {/* <div className="border rounded-xl p-4 bg-white shadow-sm">
               <div className="grid grid-cols-2 gap-4 text-left mb-4">
                 <div className="flex items-center gap-2">
                   <MastercardIcon />
@@ -151,7 +151,7 @@ export default function PaymentStatusModal({
                   </p>
                 </div>
               </div>
-            </div>
+            </div> */}
           </>
         )}
       </DialogContent>
